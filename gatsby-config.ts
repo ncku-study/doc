6c654cc1ec9b31b9c7f2a1fee896b524071c9f47
Vacantron Chen <e24081030@gs.ncku.edu.tw>
2022-08-12 22:20:45 +0800

    feat: apply gh-pages

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,9 +2,9 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `My Gatsby Site`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Documntation`,
   },
+  pathPrefix: '/doc',
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
