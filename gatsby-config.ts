e458f9a4d538faafd85dc5e6f27e1a551210ff20
Vacantron Chen <e24081030@gs.ncku.edu.tw>
2022-08-12 23:03:53 +0800

    fix: downgrade `gh-pages`

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -4,24 +4,29 @@ const config: GatsbyConfig = {
   siteMetadata: {
     title: `Documntation`,
   },
-  pathPrefix: '/doc',
+  pathPrefix: "/doc",
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-emotion", {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": ""
-    }
-  }, "gatsby-transformer-remark", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+  plugins: [
+    "gatsby-plugin-emotion",
+    // {
+    //   resolve: "gatsby-plugin-google-analytics",
+    //   options: {
+    //     trackingId: "",
+    //   },
+    // },
+    "gatsby-transformer-remark",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
     },
-    __key: "pages"
-  }]
+  ],
 };
 
 export default config;
